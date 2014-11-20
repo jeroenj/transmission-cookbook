6c0efbd3f15829dcadfb566adfb30e35d2807ced
Jeroen Jacobs <git@jeroenj.be>
2014-11-20 20:34:51 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name              'transmission'
+maintainer        'Jeroen Jacobs'
+maintainer_email  'chef@jeroenj.be'
+license           'MIT'
+description       'Installs transmission.'
+version           '1.0.0'
+
+supports          'ubuntu'
---
+++ recipes/default.rb
@@ -0,0 +1,37 @@
+%w[transmission transmission-cli transmission-daemon].each do |pkg|
+  package pkg
+end
+
+template '/etc/default/transmission-daemon' do
+  source 'transmission-daemon.default.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
+
+template '/etc/init/transmission-daemon.conf' do
+  source 'transmission-daemon.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
+
+template '/etc/init.d/transmission-daemon' do
+  source 'transmission-daemon.init.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+service 'transmission' do
+  service_name 'transmission-daemon'
+  supports restart: true, reload: true
+end
+
+file '/etc/transmission-daemon/settings.json' do
+  content JSON.generate(node[:transmission][:settings], indent: '    ', space: ' ', object_nl: "\n").gsub(/,$/, ', ')
+  owner node[:transmission][:user]
+  group node[:transmission][:group]
+  mode 0600
+  notifies :reload, 'service[transmission]', :immediate
+end
