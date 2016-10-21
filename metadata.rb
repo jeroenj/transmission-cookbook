2188c7021dcfde558dfb56e879debc4789873512
Jeroen Jacobs <git@jeroenj.be>
2016-10-21 21:05:59 +0200

    Bumps version to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'Installs transmission.'
-version           '1.0.0'
+version           '1.0.1'
 
 supports          'ubuntu'
